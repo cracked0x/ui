5688640d4322c077a988cf92d46c62bb30558b81
0xdeathstalker <soumyamukherjee.work@gmail.com>
2025-06-23 18:08:30 +0530

    update: home page text

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <div className="container mx-auto min-h-screen w-full max-w-[100ch] px-4">
-      <div className="pb-10 pt-20 font-mono">
+    <div className="container mx-auto flex flex-col justify-center min-h-screen w-full max-w-[100ch] px-4">
+      <div className="font-mono">
         <h1 className="text-brand-secondary flex flex-wrap items-center justify-center text-3xl font-bold sm:text-5xl">
-          cracked0x ui components
+          cracked0x ui library
         </h1>
 
         <p className="mt-3 max-w-[80%] place-self-center text-center text-xs leading-5 sm:mt-5 sm:text-base">
